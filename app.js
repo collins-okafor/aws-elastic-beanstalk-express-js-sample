3343757d1fa1667a0a463c02dedca4b4d88c3f19
Collins Okafor <Collinszurum@gmail.com>
2023-03-31 01:23:56 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Into the world of DevOps!"));
+app.get("/", (req, res) => res.send("Once upon a time in a very distant past"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
